d41cd62f6114385496e3b359138536603a6c0bf1
nles-crt <61862786+nles-crt@users.noreply.github.com>
2023-03-25 09:57:37 +0800

    v1..0.0
    test

--- app.js
+++ app.js
@@ -1,59 +1,89 @@
 const TelegramBot = require("node-telegram-bot-api");
 const https = require("https");
 const fs = require("fs");
-const { openaiApiRequest } = require("./test.js");
-const { getqq } = require("./test.js");
+const { generateAndReturnImage,getqq,app,addUserResponse } = require("./httpsending.js");
 const bot = new TelegramBot("机器人密钥", { polling: true });
 const options = {
   key: fs.readFileSync("/root/private.key"),
   cert: fs.readFileSync("/root/cert_chain.crt"),
 };
-const userChatHistory = {};
 function opentxt(data) {
   const stream = fs.createWriteStream("log.txt", { flags: "a" });
   stream.write(`${data}\n`);
   stream.end();
 }
+const userAccessRecords = new Map();
+// 检查用户访问记录
+function hasVisitedWithin30Seconds(userId) {
+  const currentTime = Date.now();
+  const lastVisitTime = userAccessRecords.get(userId);
+  // 如果用户没有访问记录，将其添加到记录中
+  if (!lastVisitTime) {
+    userAccessRecords.set(userId, currentTime);
+    return false;
+  }
+  const timeDifference = (currentTime - lastVisitTime) / 1000;
+  if (timeDifference <= 5) {
+    console.log(userAccessRecords)
+    return true;
+  } else {
+    // 更新访问记录
+    userAccessRecords.set(userId, currentTime);
+    console.log(userAccessRecords)
+    return false;
+  }
+}
 
-function echolog(userId, chatId, text) {
+function searchuserID(userId) {
+  return bot.getChatMember('-1001949987050', userId)  //判断用户是否在群聊一定要修改
+    .then((result) => {
+      if (result.status === 'member' || result.status === 'administrator' || result.status === 'creator') {
+        return true;
+      } else {
+        return false;
+      }
+    })
+    .catch((error) => {
+      console.error(error);  // 输出错误信息
+      throw error;  // 抛出错误
+    });
+}
+
+function echolog(userId, username, chatId, text) {
   if (chatId === "none") {
     return;
   }
-  log = `用户:${userId}|聊天id:${chatId}|内容:${text}`;
+  log = `用户:${userId}|用户名:${username}|聊天id:${chatId}|内容:${text}`;
   opentxt(log);
-  console.log(`用户:${userId}|聊天id:${chatId}|内容:${text}`);
+  console.log(`用户:${userId}|用户名:${username}|聊天id:${chatId}|内容:${text}`);
   return;
 }
 
-function updateUserChatHistory(userId, message, isUser = true) {
-
-  if (!userChatHistory[userId]) {
-    userChatHistory[userId] = [];
-  }
-  if (message == '结束对话'){
-    userChatHistory[userId] = [];
-    return true;
-  }
-
-  if(userChatHistory[userId].length > 8){   //限制储存用户内容长度大于8清空
-    userChatHistory[userId] = [];
-  }
-  console.log(userChatHistory[userId].length)
-  const prefix = isUser ? " " : "\n\n";
-  userChatHistory[userId].push(prefix + message);
-}
 
 bot.on("message", async (msg) => {
   const userId = msg.from.id;
   const chatId = msg.chat.id;
   const text = msg.text;
+  const username = msg.from.username;
+  echolog(userId, username, chatId, text);
+  if (!searchuserID(userId)) {
+    return bot.sendMessage(chatId, '你还未加入频道\n频道申请:https://t.me/')
+  };
+  if (hasVisitedWithin30Seconds(userId)) {
+    return bot.sendMessage(chatId, '您在过去5秒内已访问过');
+  }
   switch (text) {
     case '/start':
+      console.log(text)
       return bot.sendMessage(chatId, '对话开始');
+    case '/help':
+      console.log(text)
+      return bot.sendMessage(chatId, '测试:\n/start 开始对话\n/end 结束对话\n/qq qq号');
     default:
       if (text.startsWith('/qq')) {
         const regex = /\/qq\s+(\d+)/;
         const match = regex.exec(text);
+        console.log(text)
         if (match == null) { // 检查传入的参数是否符合要求
           return bot.sendMessage(chatId, '请输入正确的 QQ 号');
         }
@@ -64,23 +94,42 @@ bot.on("message", async (msg) => {
         const data = await getqq(qq);
         return bot.sendMessage(chatId, data);
       }
+
+      if (text.startsWith('/image')){
+        const regex = /\/image\s+(.+)/i;
+        const match = regex.exec(text);
+        console.log(text)
+        if (match == null) { // 检查传入的参数是否符合要求
+          return bot.sendMessage(chatId, '请输入图片要求');
+        }
+        try {
+          generateAndReturnImage(match[1])
+            .then((imageUrl) => {
+              return bot.sendPhoto(chatId, imageUrl);
+            })
+            .catch((error) => {
+              console.error(error);
+            });
+
+        }catch(error){
+          console.log(error)
+        }
+        return;
+      }
+
+
   }
-  echolog(userId, chatId, text);
-  if(updateUserChatHistory(userId, text)){
-    return bot.sendMessage(chatId,'对话已经结束');
+  if (addUserResponse(userId, text)) {
+    return bot.sendMessage(chatId, '对话已经结束,清空对话');
   }
   try {
     const thinkingMessage = await bot.sendMessage(chatId, "思考中");
     const thinkingMessageId = thinkingMessage.message_id;
-    const chatHistory = userChatHistory[userId].join("\n");
-    const inputWithHistory = chatHistory + "\n" + text;
-    const output = await openaiApiRequest(input=inputWithHistory);
+    const output = await app(userId);
     await bot.editMessageText(text + output, {
       chat_id: chatId,
       message_id: thinkingMessageId,
     });
-
-    updateUserChatHistory(userId, output, false);
   } catch (error) {
     console.error(error);
   }
---
+++ httpsending.js
@@ -0,0 +1,160 @@
+const https = require('https');
+const { Configuration, OpenAIApi } = require('openai');
+const configuration = new Configuration({
+  apiKey: 'openai key';  //设置openai key
+});
+
+const openai = new OpenAIApi(configuration);
+
+async function getChatResponse(listdata) {
+  const completion = await openai.createChatCompletion({
+    stream: true,
+    model: 'gpt-3.5-turbo',
+    messages: listdata,
+  }, { responseType: 'stream' });
+
+  let testdata = '';
+
+  completion.data.on('data', (data) => {
+    const message = data.toString().trim().replace(/^data: /, '');
+
+    if (message === '[DONE]') {
+      completion.data.removeAllListeners('data');
+      return;
+    } else {
+      try {
+        const parsed = JSON.parse(message);
+        testdata += parsed.choices[0].delta.content;
+      } catch {
+        return;
+      }
+    }
+  });
+
+  return new Promise((resolve, reject) => {
+    completion.data.on('end', () => {
+      resolve(testdata);
+    });
+    completion.data.on('error', (err) => {
+      reject(err);
+    });
+  });
+}
+
+// 储存每个用户的列表
+const userDict = {};
+
+// 添加用户输入和 AI 回答到列表中
+function addUserResponse(userId, text) {
+  // 检查该用户是否已存在于对象中
+  if (!userDict[userId]) {
+    userDict[userId] = [];
+  }
+  if (text == '结束对话' || text == '/end'){
+    userDict[userId] = [];
+    return true;
+  }
+  // 添加新的字典对象到该用户的列表中
+  userDict[userId].push({ role: 'user', content: text });
+
+  // 如果列表长度达到最大值，则删除前一半的元素
+  const MAX_LIST_LENGTH = 10;
+if (userDict[userId].length > MAX_LIST_LENGTH) {
+  const DELETE_COUNT = Math.floor(MAX_LIST_LENGTH / 2);
+  userDict[userId] = userDict[userId].slice(DELETE_COUNT);
+}
+  console.log(userDict[userId]);
+}
+
+function app(userId) {
+  const listdata = userDict[userId] || [];
+  return getChatResponse(listdata)
+    .then((response) => {
+      response = response.replace(/^undefined/, '').replace(/undefined$/, ''); // 去除字符串的 undefined
+      console.log('Chat response:', response);
+      userDict[userId].push({ role: 'assistant', content: response }); // AI 回答(通过 role 区分)
+      return response;
+    })
+    .catch((err) => {
+      console.error('Error:', err);
+    });
+}
+
+function getqq(qq) {
+    return new Promise((resolve, reject) => {
+      https.get('https://zy.xywlapi.cc/qqcx2023?qq=' + qq, (response) => {
+        let data = '';
+  
+        response.on('data', (chunk) => {
+          data += chunk;
+        });
+  
+        response.on('end', () => {
+          resolve(data); // 将请求结果传递给 resolve 函数
+        });
+      }).on('error', (error) => {
+        reject(error); // 将错误传递给 reject 函数
+      });
+    });
+  }
+
+  const OPENAI_API_KEY = 'openai key';             //设置openai key
+  const url = 'https://api.openai.com/v1/images/generations';
+  
+  function generateImage(prompt, n=1, size='512x512') {
+    const postData = JSON.stringify({
+      prompt,
+      n,
+      size,
+    });
+  
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${OPENAI_API_KEY}`,
+      },
+    };
+  
+    return new Promise((resolve, reject) => {
+      const req = https.request(url, options, (res) => {
+        let data = '';
+  
+        res.on('data', (chunk) => {
+          data += chunk;
+        });
+  
+        res.on('end', () => {
+          try {
+            const imageUrl = JSON.parse(data).data[0].url;
+            resolve(imageUrl);
+          } catch (error) {
+            reject(error);
+          }
+        });
+      });
+  
+      req.on('error', (error) => {
+        reject(error);
+      });
+  
+      req.write(postData);
+      req.end();
+    });
+  }
+  
+  async function generateAndReturnImage(prompt) {
+    try {
+      const imageUrl = await generateImage(prompt);
+      return imageUrl;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  }
+module.exports = {
+    addUserResponse,
+    app,
+    getqq,
+    generateAndReturnImage
+};
